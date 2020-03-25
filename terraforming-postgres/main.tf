cd3535b4c38adfa4f70873bce05e8dcd0af9ae98
Simon O'Brien <siobrien@pivotal.io>
2020-03-25 14:56:40 +0000

    creating new resource group for postgres

--- terraforming-postgres/main.tf
+++ terraforming-postgres/main.tf
@@ -12,37 +12,40 @@ terraform {
   required_version = "< 0.12.0"
 }
 
-data "azurerm_resource_group" "postgres_resource_group" {
-  name = "${var.env_name}"
-}
+# data "azurerm_subnet" "postgres_subnet" {
+#   name                 = "${var.postgres_subnet_name}"
+#   virtual_network_name = "${var.postgres_network_name}"
+#   resource_group_name  = "${data.azurerm_resource_group.postgres_resource_group.name}"
+# }
 
-data "azurerm_subnet" "postgres_subnet" {
-  name                 = "${var.postgres_subnet_name}"
-  virtual_network_name = "${var.postgres_network_name}"
-  resource_group_name  = "${data.azurerm_resource_group.postgres_resource_group.name}"
-}
+# data "azurerm_network_security_group" "postgres_security_group" {
+#   name                = "${var.postgres_security_group_name}"
+#   resource_group_name  = "${data.azurerm_resource_group.postgres_resource_group.name}"
+# }
 
-data "azurerm_network_security_group" "postgres_security_group" {
-  name                = "${var.postgres_security_group_name}"
-  resource_group_name  = "${data.azurerm_resource_group.postgres_resource_group.name}"
-}
+# data "azurerm_dns_zone" "postgres_dns_zone" {
+#   name                = "poc-mod.siob.me"
+#   resource_group_name  = "${data.azurerm_resource_group.postgres_resource_group.name}"
+# }
 
-data "azurerm_dns_zone" "postgres_dns_zone" {
-  name                = "poc-mod.siob.me"
-  resource_group_name  = "${data.azurerm_resource_group.postgres_resource_group.name}"
-}
 
-module "postgres" {
-  source = "../modules/postgres"
-
-  env_name = "${var.env_name}"
+resource "azurerm_resource_group" "pg_resource_group" {
+  name     = "${var.env_name}"
   location = "${var.location}"
-
-  postgres_vm_size    = "${var.postgres_vm_size}"
-  postgres_private_ip = "${cidrhost(data.azurerm_subnet.postgres_subnet.address_prefix, 5)}"
-
-  resource_group_name  = "${data.azurerm_resource_group.postgres_resource_group.name}"
-  dns_zone_name       = "${data.azurerm_dns_zone.postgres_dns_zone.name}"
-  security_group_id   = "${data.azurerm_network_security_group.postgres_security_group.id}"
-  subnet_id           = "${data.azurerm_subnet.postgres_subnet.id}"
 }
+
+
+# module "postgres" {
+#   source = "../modules/postgres"
+
+#   env_name = "${var.env_name}"
+#   location = "${var.location}"
+
+#   postgres_vm_size    = "${var.postgres_vm_size}"
+#   postgres_private_ip = "${cidrhost(data.azurerm_subnet.postgres_subnet.address_prefix, 5)}"
+
+#   resource_group_name  = "${data.azurerm_resource_group.postgres_resource_group.name}"
+#   dns_zone_name       = "${data.azurerm_dns_zone.postgres_dns_zone.name}"
+#   security_group_id   = "${data.azurerm_network_security_group.postgres_security_group.id}"
+#   subnet_id           = "${data.azurerm_subnet.postgres_subnet.id}"
+# }
