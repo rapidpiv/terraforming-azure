96a184a1e3947050f2e2d77c056d3c7364a33f15
Simon O'Brien <siobrien@pivotal.io>
2020-03-25 15:54:35 +0000

    added missing vars

---
+++ modules/postgres/variables.tf
@@ -0,0 +1,31 @@
+variable "env_name" {
+  default = ""
+}
+
+variable "location" {
+  default = ""
+}
+
+variable "postgres_private_ip" {
+  default = ""
+}
+
+variable "postgres_vm_size" {
+  default = ""
+}
+
+variable "resource_group_name" {
+  default = ""
+}
+
+variable "security_group_id" {
+  default = ""
+}
+
+variable "subnet_id" {
+  default = ""
+}
+
+variable "postgres_public_key" {
+  default = ""
+}
