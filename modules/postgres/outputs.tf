da31d36dfc92f689f30ef96c3626c740e474bdf4
Simon O'Brien <siobrien@pivotal.io>
2020-03-26 09:46:30 +0000

    updated output

--- modules/postgres/outputs.tf
+++ modules/postgres/outputs.tf
@@ -1,3 +1,3 @@
-output "postgres_public_ip" {
-  value = "${azurerm_public_ip.postgres_public_ip.ip_address}"
+output "postgres_public_ips" {
+  value = "${azurerm_public_ip.postgres_public_ip.*.ip_address}"
 }
