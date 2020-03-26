31b8aaf9f7fd7fe53e00bfee0a4bf0fe20cc2e86
Simon O'Brien <siobrien@pivotal.io>
2020-03-26 09:52:25 +0000

    updated

--- modules/postgres/postgres.tf
+++ modules/postgres/postgres.tf
@@ -28,75 +28,77 @@ resource "azurerm_public_ip" "postgres_public_ip" {
   idle_timeout_in_minutes = 30
 }
 
-resource "azurerm_network_interface" "postgres_nic" {
+# resource "azurerm_network_interface" "postgres_nic" {
 
-  count                   = "${var.postgres_vm_count}"
+#   count                   = "${var.postgres_vm_count}"
 
-  name                      = "${var.env_name}-postgres-nic-${count.index}"
-  depends_on                = ["azurerm_public_ip.postgres_public_ip.*"]
-  location                  = "${var.location}"
-  resource_group_name       = "${var.resource_group_name}"
-  network_security_group_id = "${var.security_group_id}"
-
-  ip_configuration {
-    name                          = "${var.env_name}-postgres-ip-config"
-    subnet_id                     = "${var.subnet_id}"
-    private_ip_address_allocation = "static"
-    private_ip_address            = "${var.postgres_private_ip}"
-    public_ip_address_id          = "${azurerm_public_ip.postgres_public_ip.id}"
-  }
-}
-
-resource "azurerm_virtual_machine" "postgres_vm" {
-
-  count                   = "${var.postgres_vm_count}"
+#   name                      = "${var.env_name}-postgres-nic-${count.index}"
   
-  name = "${var.env_name}-postgres-vm-${count.index}"
-  location                      = "${var.location}"
-  resource_group_name       = "${var.resource_group_name}"
-  network_interface_ids         = ["${azurerm_network_interface.postgres_nic.*.id}"]
-  vm_size                  = "${var.postgres_vm_size}"
+#   depends_on                = ["${element(azurerm_public_ip.postgres_public_ip.*, count.index)}"]
+#   # depends_on                = ["azurerm_public_ip.postgres_public_ip.*"]
+#   location                  = "${var.location}"
+#   resource_group_name       = "${var.resource_group_name}"
+#   network_security_group_id = "${var.security_group_id}"
 
-  storage_os_disk {
-    name              = "postgres-disk.vhd"
-    caching           = "ReadWrite"
-    os_type           = "linux"
-    create_option     = "FromImage"
-    disk_size_gb      = "150"
-    managed_disk_type = "Premium_LRS"
-  }
+#   ip_configuration {
+#     name                          = "${var.env_name}-postgres-ip-config"
+#     subnet_id                     = "${var.subnet_id}"
+#     private_ip_address_allocation = "static"
+#     private_ip_address            = "${var.postgres_private_ip}"
+#     public_ip_address_id          = "${azurerm_public_ip.postgres_public_ip.id}"
+#   }
+# }
 
-  os_profile {
-    computer_name  = "${var.env_name}-postgres"
-    admin_username = "pgadmin"
-  }
+# resource "azurerm_virtual_machine" "postgres_vm" {
 
-  os_profile_linux_config {
-    disable_password_authentication = true
-    ssh_keys {
-      path     = "/home/pgadmin/.ssh/authorized_keys"
-      key_data = "${var.postgres_public_key}"
-    }
-  }
+#   count                   = "${var.postgres_vm_count}"
+  
+#   name = "${var.env_name}-postgres-vm-${count.index}"
+#   location                      = "${var.location}"
+#   resource_group_name       = "${var.resource_group_name}"
+#   network_interface_ids         = ["${azurerm_network_interface.postgres_nic.*.id}"]
+#   vm_size                  = "${var.postgres_vm_size}"
 
-  storage_image_reference {
-    publisher = "OpenLogic"
-    offer     = "CentOS"
-    sku       = "7.5"
-    version   = "latest"
-  }
+#   storage_os_disk {
+#     name              = "postgres-disk.vhd"
+#     caching           = "ReadWrite"
+#     os_type           = "linux"
+#     create_option     = "FromImage"
+#     disk_size_gb      = "150"
+#     managed_disk_type = "Premium_LRS"
+#   }
 
-  tags = {
-    environment = "${var.env_name}"
-  }
+#   os_profile {
+#     computer_name  = "${var.env_name}-postgres"
+#     admin_username = "pgadmin"
+#   }
 
-  provisioner "remote-exec" {
-    inline = ["sudo yum -y install python"]
+#   os_profile_linux_config {
+#     disable_password_authentication = true
+#     ssh_keys {
+#       path     = "/home/pgadmin/.ssh/authorized_keys"
+#       key_data = "${var.postgres_public_key}"
+#     }
+#   }
 
-    connection {
-      type        = "ssh"
-      user        = "pgadmin"
-      private_key = "${var.postgres_private_key}"
-    }
-  }
+#   storage_image_reference {
+#     publisher = "OpenLogic"
+#     offer     = "CentOS"
+#     sku       = "7.5"
+#     version   = "latest"
+#   }
+
+#   tags = {
+#     environment = "${var.env_name}"
+#   }
+
+#   provisioner "remote-exec" {
+#     inline = ["sudo yum -y install python"]
+
+#     connection {
+#       type        = "ssh"
+#       user        = "pgadmin"
+#       private_key = "${var.postgres_private_key}"
+#     }
+#   }
 }
