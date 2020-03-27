5ab23fde281d298e084a8ff9d0c320007666f808
Simon O'Brien <siobrien@pivotal.io>
2020-03-27 18:54:43 +0000

    added output for hostnames

--- modules/postgres/outputs.tf
+++ modules/postgres/outputs.tf
@@ -1,7 +1,10 @@
+locals {
+  os_profiles = "${azurerm_virtual_machine.postgres_vm.*.os_profile}"
+}
 output "postgres_public_ips" {
   value = "${azurerm_public_ip.postgres_public_ip.*.ip_address}"
 }
 
 output "postgres_host_names" {
-  value = "${azurerm_virtual_machine.postgres_vm.*.os_profile[computer_name]}"
+  value = "${local.os_profiles}"
 }
