6372712dd3604dba6f4ce36a50c8430a1acf8b36
Simon O'Brien <siobrien@pivotal.io>
2020-03-27 19:08:35 +0000

    added output for hostnames

--- modules/postgres/outputs.tf
+++ modules/postgres/outputs.tf
@@ -1,12 +1,7 @@
-data "template_file" "data_id" {
-  count = "${var.postgres_vm_count}"
-  template = "${lookup(azurerm_virtual_machine.postgres_vm.*.os_profile[count.index], "computer_name")}"
-}
-
 output "postgres_public_ips" {
   value = "${azurerm_public_ip.postgres_public_ip.*.ip_address}"
 }
 
 output "postgres_host_names" {
-  value = "${data.template_file.data_id.*.rendered}"
+  value = "${azurerm_virtual_machine.postgres_vm.*.os_profile}"
 }
