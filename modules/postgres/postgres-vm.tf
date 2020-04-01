a83d51c682b0b890743c9c65ddc4bcd6586ce662
Simon O'Brien <siobrien@pivotal.io>
2020-04-01 18:52:53 +0100

    added additional ip config to pg vms

--- modules/postgres/postgres-vm.tf
+++ modules/postgres/postgres-vm.tf
@@ -20,18 +20,25 @@ resource "azurerm_network_interface" "pg_vm_nic" {
 
   count                   = "${var.postgres_vm_count}"
 
-  name                      = "${var.env_name}-pg-nic-${count.index}"
+  name                      = "${var.env_name}-pg-vm-nic-${count.index}"
   location                  = "${var.location}"
   resource_group_name       = "${var.resource_group_name}"
   network_security_group_id = "${var.security_group_id}"
 
 
   ip_configuration {
-    name                          = "${var.env_name}-pg-ip-config-${count.index}"
+    name                          = "${var.env_name}-pg-vm-rw-ip-config-${count.index}"
     subnet_id                     = "${var.subnet_id}"
     private_ip_address_allocation = "static"
     private_ip_address            = "${cidrhost(var.postgres_subnet_cidr, 5+count.index)}"
     public_ip_address_id          = "${element(azurerm_public_ip.pg_vm_public_ip.*.id, count.index)}"
+  }
+
+  ip_configuration {
+    name                          = "${var.env_name}-pg-vm-ro-ip-config-${count.index}"
+    subnet_id                     = "${var.subnet_id}"
+    private_ip_address_allocation = "static"
+    private_ip_address            = "${cidrhost(var.postgres_subnet_cidr, 10+count.index)}"
   }
 }
 
@@ -40,16 +47,16 @@ resource "azurerm_network_interface_backend_address_pool_association" "pg-vm-nic
   count                   = "${var.postgres_vm_count}"
 
   network_interface_id    = "${element(azurerm_network_interface.pg_vm_nic.*.id, count.index)}"
-  ip_configuration_name   = "${var.env_name}-pg-ip-config-${count.index}"
+  ip_configuration_name   = "${var.env_name}-pg-vm-rw-ip-config-${count.index}"
   backend_address_pool_id = "${azurerm_lb_backend_address_pool.pg-lb-rw-backend-pool.id}"
 }
 
-resource "azurerm_network_interface_backend_address_pool_association" "pg-nic-ro-backend-pool-association" {
+resource "azurerm_network_interface_backend_address_pool_association" "pg-vm-nic-ro-backend-pool-association" {
 
   count                   = "${var.postgres_vm_count}"
 
   network_interface_id    = "${element(azurerm_network_interface.pg_vm_nic.*.id, count.index)}"
-  ip_configuration_name   = "${var.env_name}-pg-ip-config-${count.index}"
+  ip_configuration_name   = "${var.env_name}-pg-vm-ro-ip-config-${count.index}"
   backend_address_pool_id = "${azurerm_lb_backend_address_pool.pg-lb-ro-backend-pool.id}"
 }
 
