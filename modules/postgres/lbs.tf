50c1238f22e5fc3a80e4e61c0d180bf9b89fad01
Simon O'Brien <siobrien@pivotal.io>
2020-04-01 15:48:40 +0100

    changed naming convention for pg nodes

--- modules/postgres/lbs.tf
+++ modules/postgres/lbs.tf
@@ -1,99 +1,99 @@
 # loadbalancer to direct traffic to master node
-resource "azurerm_public_ip" "pg-leader-lb-public-ip" {
-  name                = "pg-leader-lb-public-ip"
+resource "azurerm_public_ip" "pg-master-lb-public-ip" {
+  name                = "pg-master-lb-public-ip"
   location            = "${var.location}"
   resource_group_name = "${var.resource_group_name}"
   allocation_method   = "Static"
   sku                 = "Standard"
 }
 
-resource "azurerm_lb" "pg-leader-lb" {
-  name                = "${var.env_name}-pg-leader-lb"
+resource "azurerm_lb" "pg-master-lb" {
+  name                = "${var.env_name}-pg-master-lb"
   location            = "${var.location}"
   resource_group_name = "${var.resource_group_name}"
   sku                 = "Standard"
 
   frontend_ip_configuration = {
     name                 = "frontendip"
-    public_ip_address_id = "${azurerm_public_ip.pg-leader-lb-public-ip.id}"
+    public_ip_address_id = "${azurerm_public_ip.pg-master-lb-public-ip.id}"
   }
 }
 
-resource "azurerm_lb_backend_address_pool" "pg-leader-lb-backend-pool" {
-  name                = "pg-leader-lb-backend-pool"
+resource "azurerm_lb_backend_address_pool" "pg-master-lb-backend-pool" {
+  name                = "pg-master-lb-backend-pool"
   resource_group_name = "${var.resource_group_name}"
-  loadbalancer_id     = "${azurerm_lb.pg-leader-lb.id}"
+  loadbalancer_id     = "${azurerm_lb.pg-master-lb.id}"
 }
 
-resource "azurerm_lb_probe" "pg-leader-lb-probe" {
-  name                = "pg-leader-lb-probe"
+resource "azurerm_lb_probe" "pg-master-lb-probe" {
+  name                = "pg-master-lb-probe"
   resource_group_name = "${var.resource_group_name}"
-  loadbalancer_id     = "${azurerm_lb.pg-leader-lb.id}"
+  loadbalancer_id     = "${azurerm_lb.pg-master-lb.id}"
   protocol            = "HTTP"
   port                = 8008
   request_path        = "/replica"
 }
 
-resource "azurerm_lb_rule" "postgres-rule" {
+resource "azurerm_lb_rule" "pg-master-postgres-rule" {
   name                = "postgres-rule"
   resource_group_name = "${var.resource_group_name}"
-  loadbalancer_id     = "${azurerm_lb.pg-leader-lb.id}"
+  loadbalancer_id     = "${azurerm_lb.pg-master-lb.id}"
 
   frontend_ip_configuration_name = "frontendip"
   protocol                       = "TCP"
   frontend_port                  = 5432
   backend_port                   = 5432
 
-  backend_address_pool_id = "${azurerm_lb_backend_address_pool.pg-leader-lb-backend-pool.id}"
-  probe_id                = "${azurerm_lb_probe.pg-leader-lb-probe.id}"
+  backend_address_pool_id = "${azurerm_lb_backend_address_pool.pg-master-lb-backend-pool.id}"
+  probe_id                = "${azurerm_lb_probe.pg-master-lb-probe.id}"
 }
 
 # loadbalancer to direct traffic to secondary nodes (read only)
-resource "azurerm_public_ip" "pg-follower-lb-public-ip" {
-  name                = "pg-follower-lb-public-ip"
+resource "azurerm_public_ip" "pg-secondary-lb-public-ip" {
+  name                = "pg-secondary-lb-public-ip"
   location            = "${var.location}"
   resource_group_name = "${var.resource_group_name}"
   allocation_method   = "Static"
   sku                 = "Standard"
 }
 
-resource "azurerm_lb" "pg-follower-lb" {
-  name                = "${var.env_name}-pg-follower-lb"
+resource "azurerm_lb" "pg-secondary-lb" {
+  name                = "${var.env_name}-pg-secondary-lb"
   location            = "${var.location}"
   resource_group_name = "${var.resource_group_name}"
   sku                 = "Standard"
 
   frontend_ip_configuration = {
     name                 = "frontendip"
-    public_ip_address_id = "${azurerm_public_ip.pg-follower-lb-public-ip.id}"
+    public_ip_address_id = "${azurerm_public_ip.pg-secondary-lb-public-ip.id}"
   }
 }
 
-resource "azurerm_lb_backend_address_pool" "pg-follower-lb-backend-pool" {
-  name                = "pg-follower-lb-backend-pool"
+resource "azurerm_lb_backend_address_pool" "pg-secondary-lb-backend-pool" {
+  name                = "pg-secondary-lb-backend-pool"
   resource_group_name = "${var.resource_group_name}"
-  loadbalancer_id     = "${azurerm_lb.pg-follower-lb.id}"
+  loadbalancer_id     = "${azurerm_lb.pg-secondary-lb.id}"
 }
 
-resource "azurerm_lb_probe" "pg-follower-lb-probe" {
-  name                = "pg-follower-lb-probe"
+resource "azurerm_lb_probe" "pg-secondary-lb-probe" {
+  name                = "pg-secondary-lb-probe"
   resource_group_name = "${var.resource_group_name}"
-  loadbalancer_id     = "${azurerm_lb.pg-follower-lb.id}"
+  loadbalancer_id     = "${azurerm_lb.pg-secondary-lb.id}"
   protocol            = "HTTP"
   port                = 8008
   request_path        = "/master"
 }
 
-resource "azurerm_lb_rule" "postgres-rule" {
+resource "azurerm_lb_rule" "pg-secondary-postgres-rule" {
   name                = "postgres-rule"
   resource_group_name = "${var.resource_group_name}"
-  loadbalancer_id     = "${azurerm_lb.pg-follower-lb.id}"
+  loadbalancer_id     = "${azurerm_lb.pg-secondary-lb.id}"
 
   frontend_ip_configuration_name = "frontendip"
   protocol                       = "TCP"
   frontend_port                  = 5432
   backend_port                   = 5432
 
-  backend_address_pool_id = "${azurerm_lb_backend_address_pool.pg-follower-lb-backend-pool.id}"
-  probe_id                = "${azurerm_lb_probe.pg-follower-lb-probe.id}"
+  backend_address_pool_id = "${azurerm_lb_backend_address_pool.pg-secondary-lb-backend-pool.id}"
+  probe_id                = "${azurerm_lb_probe.pg-secondary-lb-probe.id}"
 }
