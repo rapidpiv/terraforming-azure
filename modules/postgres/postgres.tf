b2ea3dd2c9c46ff491b273f0bf531de78f8e581d
Simon O'Brien <siobrien@pivotal.io>
2020-04-01 14:26:14 +0100

    added postgres vms into lb backend pool

--- modules/postgres/postgres.tf
+++ modules/postgres/postgres.tf
@@ -47,6 +47,15 @@ resource "azurerm_network_interface" "postgres_nic" {
   }
 }
 
+resource "azurerm_network_interface_backend_address_pool_association" "pg-nic-backend-master-pool-association" {
+
+  count                   = "${var.postgres_vm_count}"
+
+  network_interface_id    = "${element(azurerm_network_interface.postgres_nic.*.id, count.index)}"
+  ip_configuration_name   = "${var.env_name}-postgres-ip-config-${count.index}"
+  backend_address_pool_id = "${azurerm_lb_backend_address_pool.pg-leader-lb-backend-pool.id}"
+}
+
 resource "azurerm_virtual_machine" "postgres_vm" {
 
   count                   = "${var.postgres_vm_count}"
