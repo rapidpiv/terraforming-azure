1e4ec529fea106f215bbc2c434061bcc4dffa1fa
Simon O'Brien <siobrien@pivotal.io>
2020-04-01 16:26:10 +0100

    reverted to single lb

--- modules/postgres/lbs.tf
+++ modules/postgres/lbs.tf
@@ -49,51 +49,51 @@ resource "azurerm_lb_rule" "pg-master-postgres-rule" {
 }
 
 # loadbalancer to direct traffic to secondary nodes (read only)
-resource "azurerm_public_ip" "pg-secondary-lb-public-ip" {
-  name                = "pg-secondary-lb-public-ip"
-  location            = "${var.location}"
-  resource_group_name = "${var.resource_group_name}"
-  allocation_method   = "Static"
-  sku                 = "Standard"
-}
+# resource "azurerm_public_ip" "pg-secondary-lb-public-ip" {
+#   name                = "pg-secondary-lb-public-ip"
+#   location            = "${var.location}"
+#   resource_group_name = "${var.resource_group_name}"
+#   allocation_method   = "Static"
+#   sku                 = "Standard"
+# }
 
-resource "azurerm_lb" "pg-secondary-lb" {
-  name                = "${var.env_name}-pg-secondary-lb"
-  location            = "${var.location}"
-  resource_group_name = "${var.resource_group_name}"
-  sku                 = "Standard"
+# resource "azurerm_lb" "pg-secondary-lb" {
+#   name                = "${var.env_name}-pg-secondary-lb"
+#   location            = "${var.location}"
+#   resource_group_name = "${var.resource_group_name}"
+#   sku                 = "Standard"
 
-  frontend_ip_configuration = {
-    name                 = "frontendip"
-    public_ip_address_id = "${azurerm_public_ip.pg-secondary-lb-public-ip.id}"
-  }
-}
+#   frontend_ip_configuration = {
+#     name                 = "frontendip"
+#     public_ip_address_id = "${azurerm_public_ip.pg-secondary-lb-public-ip.id}"
+#   }
+# }
 
-resource "azurerm_lb_backend_address_pool" "pg-secondary-lb-backend-pool" {
-  name                = "pg-secondary-lb-backend-pool"
-  resource_group_name = "${var.resource_group_name}"
-  loadbalancer_id     = "${azurerm_lb.pg-secondary-lb.id}"
-}
+# resource "azurerm_lb_backend_address_pool" "pg-secondary-lb-backend-pool" {
+#   name                = "pg-secondary-lb-backend-pool"
+#   resource_group_name = "${var.resource_group_name}"
+#   loadbalancer_id     = "${azurerm_lb.pg-master-lb.id}"
+# }
 
-resource "azurerm_lb_probe" "pg-secondary-lb-probe" {
-  name                = "pg-secondary-lb-probe"
-  resource_group_name = "${var.resource_group_name}"
-  loadbalancer_id     = "${azurerm_lb.pg-secondary-lb.id}"
-  protocol            = "HTTP"
-  port                = 8008
-  request_path        = "/master"
-}
+# resource "azurerm_lb_probe" "pg-secondary-lb-probe" {
+#   name                = "pg-secondary-lb-probe"
+#   resource_group_name = "${var.resource_group_name}"
+#   loadbalancer_id     = "${azurerm_lb.pg-secondary-lb.id}"
+#   protocol            = "HTTP"
+#   port                = 8008
+#   request_path        = "/master"
+# }
 
-resource "azurerm_lb_rule" "pg-secondary-postgres-rule" {
-  name                = "postgres-rule"
-  resource_group_name = "${var.resource_group_name}"
-  loadbalancer_id     = "${azurerm_lb.pg-secondary-lb.id}"
+# resource "azurerm_lb_rule" "pg-secondary-postgres-rule" {
+#   name                = "postgres-rule"
+#   resource_group_name = "${var.resource_group_name}"
+#   loadbalancer_id     = "${azurerm_lb.pg-secondary-lb.id}"
 
-  frontend_ip_configuration_name = "frontendip"
-  protocol                       = "TCP"
-  frontend_port                  = 5432
-  backend_port                   = 5432
+#   frontend_ip_configuration_name = "frontendip"
+#   protocol                       = "TCP"
+#   frontend_port                  = 5432
+#   backend_port                   = 5432
 
-  backend_address_pool_id = "${azurerm_lb_backend_address_pool.pg-secondary-lb-backend-pool.id}"
-  probe_id                = "${azurerm_lb_probe.pg-secondary-lb-probe.id}"
-}
+#   backend_address_pool_id = "${azurerm_lb_backend_address_pool.pg-secondary-lb-backend-pool.id}"
+#   probe_id                = "${azurerm_lb_probe.pg-secondary-lb-probe.id}"
+# }
--- modules/postgres/postgres.tf
+++ modules/postgres/postgres.tf
@@ -4,19 +4,6 @@ resource random_string "postgres_storage_account_name" {
   upper   = false
 }
 
-# resource "azurerm_storage_account" "postgres_storage_account" {
-#   name                     = "${random_string.postgres_storage_account_name.result}"
-#   resource_group_name      = "${var.resource_group_name}"
-#   location                 = "${var.location}"
-#   account_tier             = "Standard"
-#   account_replication_type = "LRS"
-
-#   tags = {
-#     environment = "${var.env_name}"
-#     account_for = "postgres"
-#   }
-# }
-
 resource "azurerm_public_ip" "postgres_public_ip" {
 
   count                   = "${var.postgres_vm_count}"
@@ -57,14 +44,14 @@ resource "azurerm_network_interface_backend_address_pool_association" "pg-nic-ba
   backend_address_pool_id = "${azurerm_lb_backend_address_pool.pg-master-lb-backend-pool.id}"
 }
 
-resource "azurerm_network_interface_backend_address_pool_association" "pg-nic-backend-secondary-pool-association" {
+# resource "azurerm_network_interface_backend_address_pool_association" "pg-nic-backend-secondary-pool-association" {
 
-  count                   = "${var.postgres_vm_count}"
+#   count                   = "${var.postgres_vm_count}"
 
-  network_interface_id    = "${element(azurerm_network_interface.postgres_nic.*.id, count.index)}"
-  ip_configuration_name   = "${var.env_name}-postgres-ip-config-${count.index}"
-  backend_address_pool_id = "${azurerm_lb_backend_address_pool.pg-secondary-lb-backend-pool.id}"
-}
+#   network_interface_id    = "${element(azurerm_network_interface.postgres_nic.*.id, count.index)}"
+#   ip_configuration_name   = "${var.env_name}-postgres-ip-config-${count.index}"
+#   backend_address_pool_id = "${azurerm_lb_backend_address_pool.pg-secondary-lb-backend-pool.id}"
+# }
 
 resource "azurerm_virtual_machine" "postgres_vm" {
 
