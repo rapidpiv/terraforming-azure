da02585fde25020494dd66171e7239626ac5e59a
Simon O'Brien <siobrien@pivotal.io>
2020-04-01 18:39:59 +0100

    fixed output refs

--- modules/postgres/outputs.tf
+++ modules/postgres/outputs.tf
@@ -1,7 +1,7 @@
 output "postgres_public_ips" {
-  value = "${azurerm_public_ip.postgres_public_ip.*.ip_address}"
+  value = "${azurerm_public_ip.pg_vm_public_ip.*.ip_address}"
 }
 
 output "postgres_host_names" {
-  value = "${azurerm_virtual_machine.postgres_vm.*.os_profile}"
+  value = "${azurerm_virtual_machine.pg_vm.*.os_profile}"
 }
